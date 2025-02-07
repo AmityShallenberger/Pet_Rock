30984ffcb15e100fda1cbbaca96c7e65eda06f95
GROUT-GOBBLER <yad49@pitt.edu>
2025-02-07 12:47:29 -0500

    Yarrick test cases.

--- PetRockProject/pet_rock_id/src/test/java/ClassTest.java
+++ PetRockProject/pet_rock_id/src/test/java/ClassTest.java
@@ -12,9 +12,44 @@ public class ClassTest {
 
         assertEquals("Amity", a.getName());
     }
+    
     @Test
     public void testSetHunger(){
         a = new PetRock("", "", 1, 1, 10);
         
     }
+    
+    @Test
+    public void testPolishRock() {
+        a = new PetRock("", "", 5, 5, 5);
+        int diminishingReturning = 0;
+         
+        a.polishRock(diminishingReturning);
+        
+        assertEquals(4, a.getHunger());
+        assertEquals(4, a.getBoredom());
+        assertEquals(6, a.getEnergy());
+        assertEquals("Happy", a.getMood());
+    }
+    
+    @Test
+    public void testUpdateMood() {
+        a = new PetRock("", "", 1, 1, 10);
+        
+        a.setBoredom(6);
+        a.setEnergy(4);
+        a.setHunger(5);
+        
+        a.updateMood();
+        
+        assertEquals("Bored", a.getMood());
+    }
+    
+    /*
+        Methods to Test.
+         - Amity: feedRock() and playRock().
+         - Will: makeSavedData() and getSavedData().
+         - Yarrick: polishRock() and updateMood().
+         - Bohan: updateStats().
+    */
 }
