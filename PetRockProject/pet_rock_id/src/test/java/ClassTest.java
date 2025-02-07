b82cc59dd9714d9c6e216ddd82239d6ac5c82b61
Bohan <Boc33@pitt.edu>
2025-02-07 14:38:13 -0500

    ReadMe and ClassTest Update

--- PetRockProject/pet_rock_id/src/test/java/ClassTest.java
+++ PetRockProject/pet_rock_id/src/test/java/ClassTest.java
@@ -5,16 +5,82 @@ import static org.junit.Assert.assertEquals;
 public class ClassTest {
     PetRock a = new PetRock("", "", 1, 1, 10);
 
-    @Test
+   @Test
     public void testAddName() {
         a = new PetRock("", "", 1, 1, 10);
         a.setName("Amity");
 
         assertEquals("Amity", a.getName());
     }
+	
     @Test
     public void testSetHunger(){
         a = new PetRock("", "", 1, 1, 10);
-        
+        a.setHunger(99);
+        assertEquals(10,a.getHunger());
+        a.setHunger(-99);
+        assertEquals(0,a.getHunger());
+
     }
+	
+    @Test
+    public void testFeedRock(){
+        a = new PetRock("", "", 5, 5, 5);
+        a.feedRock();
+        
+        //   hunger -= 2;
+        //   boredom += 1;
+        //   energy -= 1;
+        assertEquals(3, a.getHunger());
+        assertEquals(6, a.getBoredom());
+        assertEquals(4, a.getEnergy());
+    }
+	
+    @Test
+    public void testPlayRock(){
+        a = new PetRock("", "", 5, 5, 5);
+        a.playRock();
+
+        //boredom -= 3;
+        //hunger += 1;
+        //energy -= 2;
+        assertEquals(6, a.getHunger());
+        assertEquals(2, a.getBoredom());
+        assertEquals(3, a.getEnergy());
+
+    }
+	
+	@Test
+	public void testUpdateStats() {
+		a = new PetRock("", "", 1, 1, 10);
+		a.setName("BohanRock");
+		
+		a.setHunger(-1);
+		a.updateStats();
+		assertEquals(0, a.getHunger());
+		
+		a.setHunger(50);
+		a.updateStats();
+		assertEquals(10, a.getHunger());
+		
+		a.setBoredom(-1);
+		a.updateStats();
+		assertEquals(0, a.getBoredom());
+		
+		a.setBoredom(50);
+		a.updateStats();
+		assertEquals(10, a.getBoredom());
+		
+		a.setEnergy(-1);
+		a.updateStats();
+		assertEquals(0, a.getEnergy());
+		
+		a.setEnergy(50);
+		a.updateStats();
+		assertEquals(10, a.getEnergy());
+		
+		
+	}
+	
+	
 }
--- README.md
+++ README.md
@@ -1,18 +1,7 @@
-Who's doing what?
- - Yunk: Output.java , make sure something for TESTER, don't do output for random events, display() and getUserInput().
- - Amitree: randomEventGenerator(), divide updateStats into the setters, output for random events,
- - Bohungus: switch statement that replaces if-else in CONTROLLER, gameEndCheck().
- - Willard: checkSaveData() in the MODEL, polish-feed-play-updateMood, global variables.
- 
- 
-Thoughts.
-
-Why is updateMood() commented out in PetRockMain() line 82?
-
-The switch-statement for userInput should be its own method.
-The try-catch that writes to the current json file should be its
-	own method.
-
-Need to replace code in main with written methods.
-
-Could use some more comments.
+==How to Run==
+- Open NetBeans IDE.
+- Click on File at the top left corner. 
+- Select Open Project 
+- Navigate in the directory for the project folder, and navigate through the folders until you find pet_rock_id.
+- Open the pet_rock_id project.
+- At the left in the project navigator click on Source Packages, navigate to the folder pet.rock, and then run PetRockMain.java
